2508984617a257b43e9b6c554bbd8179487959fc
Lucas Salgado Vieira <116003800+Lucas-S-Vieira@users.noreply.github.com>
2025-02-17 18:11:37 -0300

    Criado os métodos para leitura do arquivo do labirinto

--- mainMaze.py
+++ mainMaze.py
@@ -10,60 +10,29 @@ def leituraMaze(arquivo):
         for linha in f:
             linha = linha.rstrip('\n')
             matriz.append(list(linha))
-    
-    mapa_vertices = {}
-    num_vertices = 0
+    linhas = len(matriz)
+    colunas = len(matriz[0])
+    grafo = listaAdjacencias.ListaAdjacencias((linhas*colunas))
     inicio = None
     fim = None
-
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            if matriz[i][j] != '#':
-                mapa_vertices[(i, j)] = num_vertices
-                if matriz[i][j] == 'S':
-                    inicio = num_vertices
-                elif matriz[i][j] == 'E':
-                    fim = num_vertices
-                num_vertices += 1
-
-    grafo = listaAdjacencias.ListaAdjacencias(num_vertices)
-
     direcoes = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    for (i, j), v1 in mapa_vertices.items():
-        for di, dj in direcoes:
-            ni, nj = i + di, j + dj
-            if (ni, nj) in mapa_vertices:
-                v2 = mapa_vertices[(ni, nj)]
-                grafo.addAresta(v1, v2)
-                grafo.addAresta(v2, v1)
-    caminho = busca.bfs(grafo, inicio, fim)
-    return grafo, inicio, fim, mapa_vertices
-
-def caminho_para_coordenadas(caminho, mapa_vertices):
-    id_para_coordenada = {v: k for k, v in mapa_vertices.items()}
-    return [id_para_coordenada[id] for id in caminho]
-
-# cria um grafo a partir de um arquivo:
-def leituraGrafo(nomeArquivo):
-    arquivo = open(nomeArquivo)
-
-    str = arquivo.readline()
-    str = str.split(" ")
-    numVertices = int(str[0])
-    numArestas = int(str[1])
-
-    grafo = listaAdjacencias.ListaAdjacencias(numVertices)
-    # grafo = matrizAdjacencias.MatrizAdjacencias(numVertices)
-
-    for i in range(numArestas):
-        str = arquivo.readline()
-        str = str.split(" ")
-        origem = int(str[0])
-        destino = int(str[1])
-        peso = int(str[2])
-        grafo.addAresta(origem, destino, peso)
-
-    return grafo
+    
+    for i in range(linhas):
+        for j in range(colunas):
+            if matriz[i][j] != '#':
+                for di, dj in direcoes:
+                    ni, nj = i+di, j+dj
+                    if nj > 0 and ni > 0:
+                        if ni < linhas and nj < colunas:
+                            if matriz[ni][nj] != '#':
+                                pai = i*colunas + j
+                                filho = ni*colunas +nj
+                                grafo.addAresta(pai,filho)
+                if matriz[i][j] == 'S':
+                    inicio = i*colunas +j
+                elif matriz[i][j] == 'E':
+                    fim = i*colunas +j
+    return grafo, inicio, fim
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -71,7 +40,8 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # sys.argv[1] contem o nome do arquivo a ser lido
-    grafo = leituraGrafo(sys.argv[1])
-
+    grafo, inicio, fim = leituraMaze(sys.argv[1])
+    caminho = busca.bfs(grafo, inicio, fim) 
     grafo.printGrafo()
+    print (caminho)
 
